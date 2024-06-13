5efcc11778a10da91658bc9f1fe6e4b7c55c2c7e
Lucas Turuta <lcas.turuta@gmail.com>
2024-06-13 18:52:42 -0300

    fix: moved the index.html to the root

--- js/productRender.js
+++ js/productRender.js
@@ -1,6 +1,24 @@
+const filterProductsByPrice = (products, minPrice, maxPrice) => {
+  minPrice = minPrice * 1;
+  maxPrice = maxPrice * 1;
+  return products.filter((p) => {
+    const price = parseInt(p.price.replace(/[Rp\s.]/g, ""));
+    return price >= minPrice && price <= maxPrice;
+  });
+};
+
+const filterProductsByName = (products) => {
+  return products.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+  });
+};
+
 fetch("/js/products.json")
   .then((res) => res.json())
   .then((products) => {
+    products = filterProductsByPrice(products, 1000, 5000000);
     const template = document.getElementById("product-template");
     const productContainer = document.getElementById("products");
 
