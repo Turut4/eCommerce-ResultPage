8e89ea2337edcbb1e58305bf0c8e49835bf405a8
Lucas Turuta <lcas.turuta@gmail.com>
2024-06-15 18:19:50 -0300

    feat: adding filtering features by price

--- js/productRender.js
+++ js/productRender.js
@@ -1,57 +1,68 @@
-const filterProductsByPrice = (products, minPrice, maxPrice) => {
+const renderProducts = (products) => {
+  const template = document.getElementById("product-template");
+  const productContainer = document.getElementById("products");
+
+  products.forEach((product) => {
+    const productEl = document.importNode(template.content, true);
+
+    productEl.querySelector(".product-name").textContent = product.name;
+    productEl.querySelector(".product-description").textContent =
+      product.description;
+    productEl.querySelector(".price").textContent = product.price;
+
+    const image = productEl.querySelector("#product-image");
+    image.src = `./img/${product.image}`;
+    image.alt = product.name;
+    if (product.old_price) {
+      productEl.querySelector(".old-price").textContent = product.old_price;
+    } else {
+      productEl.querySelector(".old-price").style.display = "none";
+    }
+
+    if (product.discount) {
+      productEl.querySelector(".discount").textContent = `-${product.discount}`;
+    } else {
+      productEl.querySelector(".discount").style.display = "none";
+    }
+
+    if (product.tag) {
+      productEl.querySelector(".tag").textContent = product.tag;
+    } else {
+      productEl.querySelector(".tag").style.display = "none";
+    }
+
+    return productContainer.appendChild(productEl);
+  });
+};
+
+const filterByPrice = (minPrice, maxPrice) => {
   minPrice = minPrice * 1;
   maxPrice = maxPrice * 1;
-  return products.filter((p) => {
-    const price = parseInt(p.price.replace(/[Rp\s.]/g, ""));
-    return price >= minPrice && price <= maxPrice;
+
+  return products.filter((product) => {
+    price = product.price.replace(/[Rp\s.]/g, "");
+    price * 1 >= minPrice && price * 1 <= maxPrice;
   });
 };
-
-const orderProductsByPrice = (products) => {
-  return products.sort((a, b) => {
-    const priceA = a.price.toUpperCase();
-    const priceB = b.price.toUpperCase();
-    return priceA < priceB ? -1 : priceA > priceB ? 1 : 0;
-  });
-};
-
-fetch("./js/products.json")
-  .then((res) => res.json())
-  .then((products) => {
-    const template = document.getElementById("product-template");
-    const productContainer = document.getElementById("products");
-
-    products.forEach((product) => {
-      const productEl = document.importNode(template.content, true);
-
-      productEl.querySelector(".product-name").textContent = product.name;
-      productEl.querySelector(".product-description").textContent =
-        product.description;
-      productEl.querySelector(".price").textContent = product.price;
-
-      const image = productEl.querySelector("#product-image");
-      image.src = `./img/${product.image}`;
-      image.alt = product.name;
-      if (product.old_price) {
-        productEl.querySelector(".old-price").textContent = product.old_price;
-      } else {
-        productEl.querySelector(".old-price").style.display = "none";
-      }
-
-      if (product.discount) {
-        productEl.querySelector(
-          ".discount"
-        ).textContent = `-${product.discount}`;
-      } else {
-        productEl.querySelector(".discount").style.display = "none";
-      }
-
-      if (product.tag) {
-        productEl.querySelector(".tag").textContent = product.tag;
-      } else {
-        productEl.querySelector(".tag").style.display = "none";
-      }
-
-      productContainer.appendChild(productEl);
+const showProducts = () =>
+  fetch("./js/products.json")
+    .then((res) => res.json())
+    .then((products) => {
+      renderProducts(products);
     });
-  });
+
+const showFilteredProducts = (minPrice, maxPrice) => {
+  fetch("./js/products.json")
+    .then((res) => res.json())
+    .then((products) => {
+      products = products.filter((product) => {
+        const price = parseFloat(product.price.replace(/[Rp\s.]/g, "")); // Handle price formatting
+        return price >= minPrice && price <= maxPrice;
+      });
+      console.log(products);
+      renderProducts(products); // Call renderProducts with filtered products
+    });
+};
+
+// showProducts();
+showFilteredProducts(10, 2000000);
